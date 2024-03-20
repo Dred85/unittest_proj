55c0a5ea8115c5651aa002eb6d9e2301ecf26e8b
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-20 21:04:18 +0300

    add get_val in dicts.py

--- utils/dicts.py
+++ utils/dicts.py
@@ -1,2 +1,13 @@
 def get_val(collection, key, default='git'):
-    pass
+    """"
+    Функция возвращает значение из словаря по переданному ключу,
+    если ключ существует.
+    В ином случае возвращается значение default
+    :collection: исходный словарь
+    :key: ключ.
+    :default: значение по ключу или по-умолчанию.
+    """
+
+    if key in collection:
+        return collection[key]
+    return default
