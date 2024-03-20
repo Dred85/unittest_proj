8cb0d58544d2456c1cb8be1d41c440f5c941d11e
Olonov Aleksandr <adrolv08111985@gmail.com>
2024-03-20 21:00:58 +0300

    fix and add tests for dicts.py

--- tests/test_dicts.py
+++ tests/test_dicts.py
@@ -5,7 +5,7 @@ from utils import dicts
 class TestArrs(unittest.TestCase):
 
     def test_get_val(self):
-        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs"), "mercurial")
+        self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs", "git"), "mercurial")
         self.assertEqual(dicts.get_val({"vcs": "mercurial"}, "vcs"), "mercurial")
         self.assertEqual(dicts.get_val({}, "vcs"), "git")
 
